6f440b1b2a051860b816284e73be21315ce7a4f4
PrasiddhShah <47950689+PrasiddhShah@users.noreply.github.com>
2026-08-03 15:51:31 -0400

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.java
@@ -0,0 +1,32 @@
+class MyStack {
+    Deque<Integer> deque;
+
+    public MyStack() {
+        deque = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        deque.addFirst(x);
+    }
+    
+    public int pop() {
+        return deque.pollFirst();
+    }
+    
+    public int top() {
+        return deque.peekFirst();
+    }
+    
+    public boolean empty() {
+        return deque.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
